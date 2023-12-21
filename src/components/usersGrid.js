4e70a29d50e8e68ddfddde2b95b5f434ad0e7bd6
HadiAfzalizadeh <hadiafzalizadeh@gmail.com>
2023-12-21 10:16:55 +0330

    formik and yup added to add user form

--- src/components/usersGrid.js
+++ src/components/usersGrid.js
@@ -15,61 +15,25 @@ import {
   Tooltip,
   Input,
 } from '@material-tailwind/react';
-import { PencilIcon } from '@heroicons/react/24/solid';
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 
 function UsersGrid() {
-  const TABLE_HEAD = ['Transaction', 'Amount', 'Date', 'Status', 'Account', ''];
+  const TABLE_HEAD = [
+    'عملیات',
+    'نام کاربری',
+    'ایمیل',
+    'کد ملی',
+    'نام خانوادگی',
+    'نام',
+  ];
 
   const TABLE_ROWS = [
     {
-      img: 'https://docs.material-tailwind.com/img/logos/logo-spotify.svg',
-      name: 'Spotify',
-      amount: '$2,500',
-      date: 'Wed 3:00pm',
-      status: 'paid',
-      account: 'visa',
-      accountNumber: '1234',
-      expiry: '06/2026',
-    },
-    {
-      img: 'https://docs.material-tailwind.com/img/logos/logo-amazon.svg',
-      name: 'Amazon',
-      amount: '$5,000',
-      date: 'Wed 1:00pm',
-      status: 'paid',
-      account: 'master-card',
-      accountNumber: '1234',
-      expiry: '06/2026',
-    },
-    {
-      img: 'https://docs.material-tailwind.com/img/logos/logo-pinterest.svg',
-      name: 'Pinterest',
-      amount: '$3,400',
-      date: 'Mon 7:40pm',
-      status: 'pending',
-      account: 'master-card',
-      accountNumber: '1234',
-      expiry: '06/2026',
-    },
-    {
-      img: 'https://docs.material-tailwind.com/img/logos/logo-google.svg',
-      name: 'Google',
-      amount: '$1,000',
-      date: 'Wed 5:00pm',
-      status: 'paid',
-      account: 'visa',
-      accountNumber: '1234',
-      expiry: '06/2026',
-    },
-    {
-      img: 'https://docs.material-tailwind.com/img/logos/logo-netflix.svg',
-      name: 'netflix',
-      amount: '$14,000',
-      date: 'Wed 3:30am',
-      status: 'cancelled',
-      account: 'visa',
-      accountNumber: '1234',
-      expiry: '06/2026',
+      name: 'namefdf',
+      surname: 'surnamefdff',
+      melicode: 'melicode1212',
+      username: 'usernamesddfss',
+      email: 'emailfdf',
     },
   ];
 
@@ -77,24 +41,32 @@ function UsersGrid() {
     <Card className="h-full w-full">
       <CardHeader floated={false} shadow={false} className="rounded-none">
         <div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-center">
-          <div>
-            <Typography variant="h5" color="blue-gray">
-              Recent Transactions
-            </Typography>
-            <Typography color="gray" className="mt-1 font-normal">
-              These are details about the last transactions
-            </Typography>
-          </div>
           <div className="flex w-full shrink-0 gap-2 md:w-max">
             <div className="w-full md:w-72">
               <Input
-                label="Search"
-                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                dir="rtl"
+                // label="Search"
+                // icon={<MagnifyingGlassIcon className="h-5 w-5" />}
               />
+              <div class="md:col-span-5 text-right">
+                <div class="inline-flex items-end">
+                  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    جست و جو
+                  </button>
+                </div>
+              </div>
             </div>
             <Button className="flex items-center gap-3" size="sm">
               <ArrowDownTrayIcon strokeWidth={2} className="h-4 w-4" /> Download
             </Button>
+          </div>
+        </div>
+
+        <div class="md:col-span-5 text-right">
+          <div class="inline-flex items-end">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              جدید
+            </button>
           </div>
         </div>
       </CardHeader>
@@ -120,19 +92,7 @@ function UsersGrid() {
           </thead>
           <tbody>
             {TABLE_ROWS.map(
-              (
-                {
-                  img,
-                  name,
-                  amount,
-                  date,
-                  status,
-                  account,
-                  accountNumber,
-                  expiry,
-                },
-                index
-              ) => {
+              ({ name, surname, melicode, username, email }, index) => {
                 const isLast = index === TABLE_ROWS.length - 1;
                 const classes = isLast
                   ? 'p-4'
@@ -141,95 +101,61 @@ function UsersGrid() {
                 return (
                   <tr key={name}>
                     <td className={classes}>
-                      <div className="flex items-center gap-3">
-                        <Avatar
-                          src={img}
-                          alt={name}
-                          size="md"
-                          className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
-                        />
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-bold"
-                        >
-                          {name}
-                        </Typography>
-                      </div>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {amount}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {date}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <div className="w-max">
-                        <Chip
-                          size="sm"
-                          variant="ghost"
-                          value={status}
-                          color={
-                            status === 'paid'
-                              ? 'green'
-                              : status === 'pending'
-                              ? 'amber'
-                              : 'red'
-                          }
-                        />
-                      </div>
-                    </td>
-                    <td className={classes}>
-                      <div className="flex items-center gap-3">
-                        <div className="h-9 w-12 rounded-md border border-blue-gray-50 p-1">
-                          <Avatar
-                            src={
-                              account === 'visa'
-                                ? 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/visa.png'
-                                : 'https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/logos/mastercard.png'
-                            }
-                            size="sm"
-                            alt={account}
-                            variant="square"
-                            className="h-full w-full object-contain p-1"
-                          />
-                        </div>
-                        <div className="flex flex-col">
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal capitalize"
-                          >
-                            {account.split('-').join(' ')} {accountNumber}
-                          </Typography>
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal opacity-70"
-                          >
-                            {expiry}
-                          </Typography>
-                        </div>
-                      </div>
-                    </td>
-                    <td className={classes}>
                       <Tooltip content="Edit User">
                         <IconButton variant="text">
-                          <PencilIcon className="h-4 w-4" />
+                          <PencilIcon className="h-4 w-4 m-1" />
                         </IconButton>
                       </Tooltip>
+                      <Tooltip content="Delete User">
+                        <IconButton variant="text">
+                          <TrashIcon className="h-4 w-4 m-1" />
+                        </IconButton>
+                      </Tooltip>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {username}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {email}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {melicode}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {surname}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {name}
+                      </Typography>
                     </td>
                   </tr>
                 );
